888745fa4154227e7898ad239492c6abb51ca4b6
Suzal3579 <pe.messh@gmail.com>
2017-08-06 14:14:53 +0530

    UI changes .. Using fetch api for quotes .... And some minor change in skills ...

--- main.js
+++ main.js
@@ -1,9 +1,42 @@
 $(document).ready(() => {
-    $('.container').hide()
+    const quote = $('#quote'),
+        author = $('#author'),
+        anotherQuote = $('#anotherQuote'),
+        blockQuote = $('.blockquote-reverse'),
+        container = $('.container')
+
+    container.hide()
+    blockQuote.hide()
     setInterval(() => {
         $('canvas').hide()
         $('.container').fadeIn(1000)
     }, 1200)
+
+    const quoteFun = () => {
+        fetch("https://favqs.com/api/qotd")
+            .then((response) => {
+                return response.json()
+            })
+            .then((data) => {
+                const quoteFetch = data.quote.body,
+                    quoteAuthor = data.quote.author
+
+                quote.html(quoteFetch)
+                author.html(quoteAuthor)
+                anotherQuote.html("<h6>Don't like the quote? Click here and wait !</h6>")
+                blockQuote.slideDown()
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+    }
+
+    quoteFun()
+
+    anotherQuote.on('click', (e) => {
+        quoteFun()
+    });
+
 
 })
 
